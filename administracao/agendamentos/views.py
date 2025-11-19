604e53617e74a2787f762f201d8b3a90c394976a
Victorkaue333 <kaue.alves.pg@gmail.com>
2025-11-19 10:08:46 -0300

    Update VK
    Implementação da tela de login, base e home... apenas a de login está funcional por enquanto e foram efetuadas alterações no pasta base

--- administracao/agendamentos/views.py
+++ administracao/agendamentos/views.py
@@ -1,3 +1,8 @@
+# View simples para exibir a página de agendamentos
+from django.shortcuts import render
+
+def agendamentos(request):
+	return render(request, 'agendamentos/agendamentos.html')
 from django.shortcuts import render
 
 # Create your views here.
