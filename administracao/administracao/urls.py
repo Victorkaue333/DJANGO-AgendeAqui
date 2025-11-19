54207ee4d74a302d8193bc8862c47935bd73ae32
Victorkaue333 <kaue.alves.pg@gmail.com>
2025-11-19 12:06:10 -0300

    Update Vk
    Implementação de landinpage

--- administracao/administracao/urls.py
+++ administracao/administracao/urls.py
@@ -2,13 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 from usuarios.forms import CustomAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('', auth_views.LoginView.as_view(template_name='login.html', authentication_form=CustomAuthenticationForm), name='root_login'),
+    path('', TemplateView.as_view(template_name='landinpage.html'), name='home'),
     path('dashboard/', include('dashboard.urls')),
     path('salas/', include('salas.urls')),
     path('relatorios/', include('relatorios.urls')),
