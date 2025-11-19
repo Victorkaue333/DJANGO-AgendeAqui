cf8f9c2ec781d99fad24f462b223b899906f2071
Victorkaue333 <kaue.alves.pg@gmail.com>
2025-11-19 11:32:50 -0300

    Update VK
    Implementação da tela de cadastro, alterações na tela de login e no documento base; agora precisa apenas de correções de ux

--- administracao/administracao/urls.py
+++ administracao/administracao/urls.py
@@ -2,14 +2,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from usuarios.forms import CustomAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='root_login'),
+    path('', auth_views.LoginView.as_view(template_name='login.html', authentication_form=CustomAuthenticationForm), name='root_login'),
     path('dashboard/', include('dashboard.urls')),
     path('salas/', include('salas.urls')),
     path('relatorios/', include('relatorios.urls')),
     path('agendamentos/', include('agendamentos.urls')),
+    path('', include('usuarios.urls')),
 ]
